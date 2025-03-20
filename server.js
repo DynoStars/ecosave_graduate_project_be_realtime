7c5fd4d514522f2a3a82c81f1b85c2884ade2152
Bao Pham <bao.pham@sapotacorp.vn>
2025-03-20 23:16:11 +0700

    change get prodcut.create

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const redis = new Redis({
     port: process.env.REDIS_PORT,
     password: process.env.REDIS_PASSWORD,
 });
+
 // Lắng nghe sự kiện từ Laravel qua Redis
 redis.subscribe("products", (err, count) => {
     if (err) {
@@ -43,22 +44,8 @@ redis.psubscribe("private-products", (err, count) => {
 redis.on("message", (channel, message) => {
     console.log(`Nhận sự kiện từ Redis (kênh: ${channel}):`, message);
 
-    try {
-        const parsedMessage = JSON.parse(message);
-        if (parsedMessage.data && parsedMessage.data.command) {
-            const commandData = JSON.parse(parsedMessage.data.command);
-            if (commandData.event && commandData.event.product) {
-                io.emit("product.created", commandData.event.product);
-                console.log("Đã phát sự kiện product.created:", commandData.event.product);
-            } else {
-                console.error("Không tìm thấy dữ liệu sản phẩm trong sự kiện.");
-            }
-        } else {
-            console.error("Dữ liệu Redis không hợp lệ.");
-        }
-    } catch (error) {
-        console.error("Lỗi khi phân tích dữ liệu Redis:", error);
-    }
+    const eventData = JSON.parse(message);
+    io.emit("product.created", eventData);
 });
 
 
